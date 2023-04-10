aa6b1d8f6144025a38d2bd7f8126e9582e3c4743
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-10 22:44:15 +0700

    Meta Description and Finishing CSS

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,68 +7,45 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //
-
+    public $products = [
+        [
+            "id" => 1,
+            "name" => "Bakso Aci",
+            "description" => "Bakso Aci adalah bakso yang dibuat dari tepung. Diolah dengan sepenuh hati untuk mencapai keutuhan sejati dalam jiwa ini",
+            "status" => "S",
+            "price" => 100000
+        ],
+        [
+            "id" => 2,
+            "name" => "Bakso Bali",
+            "description" => "Bakso Bali merupakan bakso yang berasal dari Bali. Sejak dahulu selalu menjadi kebanggaan rakyat Bali ketika mengadakan pesta",
+            "status" => "SR",
+            "price" => 200000
+        ],
+        [
+            "id" => 3,
+            "name" => "Bakso Cecep",
+            "description" => "Bakso Cecep adalah bakso terkenal dari Bandung yang dibuat oleh orang Sunda bernama Cecep. Beliau sangat senang membuat bakso sedari muda",
+            "status" => "SSR",
+            "price" => 300000
+        ],
+        [
+            "id" => 4,
+            "name" => "Bakso Dodi",
+            "description" => "Bakso Dodi adalah bakso terenak yang pernah dimakan oleh keluarganya. Dodi ketika membuat bakso pasti sepenuh hati dan penuh rasa syukur",
+            "status" => "S",
+            "price" => 400000
+        ],
+    ];
 
     public function index(){
-        $products = [
-            [
-                "id" => 1,
-                "name" => "Bakso Aci",
-                "status" => "S",
-                "price" => 100000
-            ],
-            [
-                "id" => 2,
-                "name" => "Bakso Bali",
-                "status" => "SR",
-                "price" => 200000
-            ],
-            [
-                "id" => 3,
-                "name" => "Bakso Cecep",
-                "status" => "SSR",
-                "price" => 300000
-            ],
-            [
-                "id" => 4,
-                "name" => "Bakso Dodi",
-                "status" => "S",
-                "price" => 400000
-            ],
-        ];
-        return view('home', ["products" => $products]);
+        
+        return view('home', ["products" => $this->products]);
     }
 
     public function single($id){
-        $products = [
-            [
-                "id" => 1,
-                "name" => "Bakso Aci",
-                "status" => "S",
-                "price" => 100000
-            ],
-            [
-                "id" => 2,
-                "name" => "Bakso Bali",
-                "status" => "SR",
-                "price" => 200000
-            ],
-            [
-                "id" => 3,
-                "name" => "Bakso Cecep",
-                "status" => "SSR",
-                "price" => 300000
-            ],
-            [
-                "id" => 4,
-                "name" => "Bakso Dodi",
-                "status" => "S",
-                "price" => 400000
-            ],
-        ];
-
         $product = [];
-        foreach($products as $p){
+        foreach($this->products as $p){
             if($p["id"] == $id){
                 $product = $p;
             }
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -4,10 +4,15 @@
     {{ $product["name"] }}
 @endsection
 
+@section('description')
+    {{ $product["description"] }}
+@endsection
+
 @section('body')
 <div class="singleproduct {{ $product["status"] }}">
     <h1>{{ $product["name"] }}</h1>
     <p>Status: {{ $product["status"] }}</p>
     <p>Price: {{ $product["price"] }}</p>
+    <p>{{ $product["description"] }}</p>
 </div>
 @endsection
